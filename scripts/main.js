9e73002c89acf10bad3a32330b57839a6fc4f9a0
Martin Loic <loic.martin2000@gmail.com>
2024-06-17 10:39:01 +0200

    Choisir mots ou phrases

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
-
 let inputUser = document.querySelector("inputEcriture");
 let validationBtn = document.getElementById("btnValiderMot");
 let motPropose = document.querySelector(".zoneProposition");
 let zoneScore = document.querySelector(".zoneScore");
-let radioBtn = document.querySelectorAll(".optionSource input");
 
 lancerJeu();
 
--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,39 @@ let score = 0;
 let choixListe;
 let motUtilisateur;
 let i = 0;
+let listeProposition = listeMots;
 
 function afficherResultat(score, nombreQuestions) {
     zoneScore.textContent = `${score} | ${nombreQuestions}`;
 }
 
-let zoneProposition = document.querySelector(".zoneProposition")
+let zoneProposition = document.querySelector(".zoneProposition");
+
 function afficherProposition(mot) {
     zoneProposition.innerHTML = mot;
 };
+
+afficherProposition("Choisissez votre option au-dessus");
+
+let radioBtn = document.querySelectorAll(".optionSource input");
+console.log(radioBtn)
+
+
+for (let j = 0; j < radioBtn.length; j++) {
+    radioBtn[j].addEventListener("change", (event) => {
+        if (event.target.value === "1") {
+            listeProposition = listeMots;
+        } else {
+            listeProposition = listePhrases;
+        }
+
+        if (listeProposition[i] === undefined) {
+            afficherProposition("Le jeu est fini");
+        } else {
+            afficherProposition(listeProposition[i]);
+        }
+    });
+}
 
 function lancerJeu() {
     let btnValider = document.getElementById("btnValiderMot");
@@ -20,12 +44,12 @@ function lancerJeu() {
         if (inputEcriture.value === zoneProposition.innerHTML) {
             score++;
         };
-        afficherResultat(score, i);
-        afficherProposition(listeMots[i]);
+        afficherResultat(score, i + 1);
+        afficherProposition(listeProposition[i + 1]);
         i++;
         inputEcriture.value = "";
 
-        if(listeMots[i - 1] === undefined) {
+        if (listeProposition[i] === undefined) {
             afficherProposition("Le jeu est fini");
             btnValider.disabled = true;
         }
